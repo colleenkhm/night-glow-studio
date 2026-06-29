08136b6602fc369d9aed515069e6a82d7cd34ce9
colleenkhm <colleenkhm@users.noreply.github.com>
2026-06-29 17:04:19 +0000

    deploy: 643e78afbbd9a550dc98b035a36c0c315a8fbc58

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2633756491" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1380173859" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
